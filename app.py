c461d70689423442099e683fdba095e9c69b81a5
rendurdreams <rendurslogin@gmail.com>
2023-11-28 13:35:09 -0500

    Added Flask app/ data is fetching from coinmarketcap api to my sql server/ Flask app is now pulling from the local My Sql./ need to format and add styles for data to display properly

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, jsonify
+import mysql.connector
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/get_data')
+def get_data():
+    conn = mysql.connector.connect(user='rendur', password='[vt%WMQX-$x2SR/', host='localhost', database='crypto_data')
+    cursor = conn.cursor(dictionary=True)
+
+    cursor.execute("SELECT * FROM cryptocurrencies")
+    result = cursor.fetchall()
+
+    cursor.close()
+    conn.close()
+
+    return jsonify(result)
+
+if __name__ == '__main__':
+    app.run(debug=True)
